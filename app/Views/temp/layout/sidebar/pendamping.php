ba128086a0a78bf2ec30ca00ecb86427a476f3fd
stefanfredik <stefanfredik@gmail.com>
2022-12-17 21:31:17 +0800

    revisi laporan

--- app/Views/temp/layout/sidebar/pendamping.php
+++ app/Views/temp/layout/sidebar/pendamping.php
@@ -1,5 +1,13 @@
 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
     <li class="nav-item">
+        <a href="/dashboard" class="nav-link <?= url_is('/home') ? 'active' : '' ?>">
+            <i class="bi bi-house-door"></i>
+            <p>
+                Dashboard
+            </p>
+        </a>
+    </li>
+    <li class="nav-item">
         <a href="/laporan/peserta  " class="nav-link  <?= @($url == 'user') ? 'active' : '' ?>">
             <i class="bi bi-people-fill nav-icon"></i>
             <p>
